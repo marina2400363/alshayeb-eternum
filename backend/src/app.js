48c9dfa6389d69c2bf8a2e5667ec025bb1bc02e6
Marina <marina2400363@miuegypt.edu.eg>
2026-06-17 21:49:46 +0300

    background adjustments

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,8 +20,10 @@ const defaultClientOrigins = [
   "https://alshayebexperience.com",
   "http://localhost:3000",
   "http://localhost:3001",
+  "http://localhost:3005",
   "http://127.0.0.1:3000",
-  "http://127.0.0.1:3001"
+  "http://127.0.0.1:3001",
+  "http://127.0.0.1:3005"
 ];
 
 const allowedOrigins = String(process.env.CLIENT_ORIGIN || defaultClientOrigins.join(","))
